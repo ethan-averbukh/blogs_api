0743529475d6fff6fc35e81edc45ef8cd081f72e
Ethan Averbukh <averbukh.ethan@gmail.com>
2022-03-24 16:43:33 -0600

    Error using kebab-case, switched to snake_case

--- api/serializers/user.py
+++ api/serializers/user.py
@@ -8,7 +8,7 @@ class UserSerializer(serializers.ModelSerializer):
         model = get_user_model()
         fields = ('id', 'email', 'password')
 
-        extra_kwargs = {'password': {'write-only': True, 'min-length': 8}}
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 8}}
 
     def create(self,validated_data):
         return get_user_model().objects.create_user(**validated_data)
