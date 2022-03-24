1f2c0c19875d4eebb9ccb0076360e06432034298
Ethan Averbukh <averbukh.ethan@gmail.com>
2022-03-24 16:46:02 -0600

    Added paths for user signup,in,out,passwordchange; Added path for blogs and blog/:id

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,16 @@
+from ast import Pass
 from xml.etree.ElementInclude import include
 from django.urls import path, include
 from .views import index
-
+from .views.users import SignIn, SignOut, SignUp, PasswordChange
+from .views.blogs import BlogsView, BlogView
 urlpatterns = [
-    path('', index.index, name='index')
+    path('', index.index, name='index'),
+    path('sign-up/', SignUp.as_view(), name='sign-up'),
+    path('sign-in/', SignIn.as_view(), name='sign-in'),
+    path('sign-out/', SignOut.as_view(), name='sign-out'),
+    path('change-password/', PasswordChange.as_view(), name='chg-password'),
+    path('blogs/', BlogsView.as_view(), name='blogs'),
+    path('blogs/<int:pk>', BlogView.as_view(), name='blog')
+
 ]
